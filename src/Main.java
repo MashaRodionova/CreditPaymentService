b7f83d1fa75b818e9219436f8567e612c690feca
Masha Rodionova <rodionovamv@gmai.com>
2022-05-24 11:24:30 +0300

    MothlyPayment32262

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double mP = service.calculate(1000000, 24, 9.99);
+        double mP = service.calculate(1000000, 36, 9.99);
         int result = (int) Math.round(mP);
         System.out.println("Ежемесячный платеж составляет " + result + " руб.");
 
